4d6511edee014dab263225d461ccdd344f20d7b0
Grady Shelton and Sowmya Dinavahi <grady@>
2017-05-25 13:52:23 -0700

    add initialize function to the backend

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
-
 function Thing() {
 
 }
@@ -18,10 +16,49 @@ exports.googleModule = Thing;
 },{}],2:[function(require,module,exports){
 var Thing = require('./../js/google.js').googleModule;
 
-$(document).ready(function() {
-  $("#submit").click(locateUser);
-  $("#locateAddress").click(locateAddress);
-});
+  $(document).ready(function() {
+    var map;
+    function initialize() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: {lat: 45.5231, lng: -122.6765},
+        mapTypeId: 'terrain'
+      });
+    }
+    initialize(map);
+    $("#submit").click(locateUser);
+    $("#locateAddress").click(locateAddress);
+
+  });
+
+// var script = document.createElement('script');
+// script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+// document.getElementsByTagName('head')[0].appendChild(script);
+//
+// map.data.setStyle(function(feature) {
+//   var magnitude = feature.getProperty('mag');
+//   return {
+//     icon: getCircle(magnitude)
+//   };
+// });
+//
+// function getCircle(magnitude) {
+//   return {
+//     path: google.maps.SymbolPath.CIRCLE,
+//     fillColor: 'red',
+//     fillOpacity: .2,
+//     scale: Math.pow(2, magnitude) / 2,
+//     strokeColor: 'white',
+//     strokeWeight: .5
+//   };
+// }
+//
+//
+// function eqfeed_callback(results) {
+//   map.data.addGeoJson(results);
+// }
+
+
 
 // ------Find Current Address
 
@@ -43,7 +80,7 @@ function geolocationSuccess(position) {
   var myOptions = {
     zoom : 16,
     center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
+    mapTypeId : google.maps.MapTypeId.TERRAIN
   };
   var mapObject = new google.maps.Map(document.getElementById('map'), myOptions);
 
@@ -80,7 +117,7 @@ function NewPosition(position) {
     var myOptions = {
       zoom : 16,
       center : userLatLng,
-      mapTypeId : google.maps.MapTypeId.ROADMAP
+      mapTypeId : google.maps.MapTypeId.TERRAIN
     };
     var mapObject = new google.maps.Map(document.getElementById('map'), myOptions);
 
--- js/google-interface.js
+++ js/google-interface.js
@@ -1,9 +1,48 @@
 var Thing = require('./../js/google.js').googleModule;
 
-$(document).ready(function() {
-  $("#submit").click(locateUser);
-  $("#locateAddress").click(locateAddress);
-});
+  $(document).ready(function() {
+    var map;
+    function initialize() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: {lat: 45.5231, lng: -122.6765},
+        mapTypeId: 'terrain'
+      });
+    }
+    initialize(map);
+    $("#submit").click(locateUser);
+    $("#locateAddress").click(locateAddress);
+
+  });
+
+// var script = document.createElement('script');
+// script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+// document.getElementsByTagName('head')[0].appendChild(script);
+//
+// map.data.setStyle(function(feature) {
+//   var magnitude = feature.getProperty('mag');
+//   return {
+//     icon: getCircle(magnitude)
+//   };
+// });
+//
+// function getCircle(magnitude) {
+//   return {
+//     path: google.maps.SymbolPath.CIRCLE,
+//     fillColor: 'red',
+//     fillOpacity: .2,
+//     scale: Math.pow(2, magnitude) / 2,
+//     strokeColor: 'white',
+//     strokeWeight: .5
+//   };
+// }
+//
+//
+// function eqfeed_callback(results) {
+//   map.data.addGeoJson(results);
+// }
+
+
 
 // ------Find Current Address
 
@@ -25,7 +64,7 @@ function geolocationSuccess(position) {
   var myOptions = {
     zoom : 16,
     center : userLatLng,
-    mapTypeId : google.maps.MapTypeId.ROADMAP
+    mapTypeId : google.maps.MapTypeId.TERRAIN
   };
   var mapObject = new google.maps.Map(document.getElementById('map'), myOptions);
 
@@ -62,7 +101,7 @@ function NewPosition(position) {
     var myOptions = {
       zoom : 16,
       center : userLatLng,
-      mapTypeId : google.maps.MapTypeId.ROADMAP
+      mapTypeId : google.maps.MapTypeId.TERRAIN
     };
     var mapObject = new google.maps.Map(document.getElementById('map'), myOptions);
 
